038043b255b8e68fb3e0d1179d4511a4be271488
yanhard <126759091+yanhard@users.noreply.github.com>
2023-05-17 09:19:47 +0700

    add iteration into 13

--- src/main/java/com/iftgroup/jobportal/config/PasswordEncoderConfig.java
+++ src/main/java/com/iftgroup/jobportal/config/PasswordEncoderConfig.java
@@ -9,6 +9,6 @@ public class PasswordEncoderConfig {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(13);
     }
 }
