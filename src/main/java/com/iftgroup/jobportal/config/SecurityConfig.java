c88afa7588b694097e5d2656356bd6ef366731d8
yanhard <126759091+yanhard@users.noreply.github.com>
2023-05-17 09:20:56 +0700

    enable csrf

--- src/main/java/com/iftgroup/jobportal/config/SecurityConfig.java
+++ src/main/java/com/iftgroup/jobportal/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.iftgroup.jobportal.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -13,6 +14,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,6 +25,9 @@ import java.util.Arrays;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 import com.iftgroup.jobportal.service.impl.UserDetailsServiceImpl;
+
+import org.springframework.http.HttpMethod;
+
 
 @Configuration
 @Import(PasswordEncoderConfig.class)
@@ -39,22 +46,40 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        RequestMatcher publicUrls = new OrRequestMatcher(
+            new AntPathRequestMatcher("/api/auth/register", "POST"),
+            new AntPathRequestMatcher("/api/auth/login", "POST"),
+            new AntPathRequestMatcher("/swagger-ui/**"),
+            new AntPathRequestMatcher("/swagger-resources/**"),
+            new AntPathRequestMatcher("/swagger-ui.html"),
+            new AntPathRequestMatcher("/v3/api-docs"),
+            new AntPathRequestMatcher("/v2/api-docs"),
+            new AntPathRequestMatcher("/webjars/**")
+        );
+    
+        RequestMatcher protectedUrls = new OrRequestMatcher(
+            new AntPathRequestMatcher("/api/**")
+        );
+    
         http
-                .csrf(csrf -> csrf
-                        .disable())
-                .authorizeRequests(authorize -> authorize
-                .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
-                        .requestMatchers("/api/**").authenticated()
-                        .anyRequest().permitAll())
-                .httpBasic(withDefaults())
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+            .csrf()
+            .and()
+            .cors().configurationSource(corsConfigurationSource()).and()
+            .authorizeRequests()
+                .requestMatchers(publicUrls).permitAll()
+                .requestMatchers(protectedUrls).authenticated()
+                .anyRequest().permitAll()
+            .and()
+            .httpBasic()
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
         return http.build();
     }
-
+                
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("*")); // domain yg diizinkan utk akses
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
